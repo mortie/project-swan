c90512a9c97f39392f4c3e615f53899e814d8dce
Martin Dørum <martid0311@gmail.com>
2020-01-08 09:36:10 +0100

    ItemStack ctor stuff

--- libswan/include/swan/ItemStack.h
+++ libswan/include/swan/ItemStack.h
@@ -6,7 +6,7 @@ class Item;
 
 class ItemStack {
 public:
-	ItemStack(Item *item, int count = 0): item_(item), count_(count) {
+	ItemStack(Item *item, int count): item_(item), count_(count) {
 
 		// We don't want a "partially empty" state.
 		if (item == nullptr || count == 0) {
@@ -14,6 +14,8 @@ public:
 			count_ = 0;
 		}
 	}
+
+	ItemStack(): item_(nullptr), count_(0) {}
 
 	Item *item() { return item_; }
 	int count() { return count_; }
