07360ef7f9868ed1a46aabde9fa6217764ead8c5
Martin Dørum <martid0311@gmail.com>
2023-03-20 12:26:59 +0100

    don't compile tracy when tracing is not enabled

--- libswan/include/swan/common.h
+++ libswan/include/swan/common.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// We want every file to be able to easily add Tracy zones
-#include <tracy/Tracy.hpp>
-
+#include <swan-common/trace.h>
 #include <swan-common/Vector2.h>
 #include <swan-common/constants.h>
 
