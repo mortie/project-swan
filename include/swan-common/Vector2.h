b0ed442cde24aa88a80c891f373883b63dcd6be1
Martin Dørum <martid0311@gmail.com>
2023-12-29 17:57:24 +0100

    fix vector hash implementation on gcc

--- include/swan-common/Vector2.h
+++ include/swan-common/Vector2.h
@@ -176,10 +176,14 @@ using Vec2i = Vector2<int>;
 
 }
 
+namespace std {
+
 template<typename T>
-struct ::std::hash<SwanCommon::Vector2<T> > {
+struct hash<SwanCommon::Vector2<T> > {
 	std::size_t operator()(const SwanCommon::Vector2<T> &vec) const
 	{
 		return std::hash<T>{}(vec.x) ^ std::hash<T>{}(vec.y);
 	}
 };
+
+}
